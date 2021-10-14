44445b421501dce85c8cf282bcc0a20ac68afc96
mbalazy <p@mbalazy.pl>
2021-10-14 18:20:34 +0200

    Add simple form with email send to sever

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,20 +1,48 @@
 import { MainStyle, MainWrapper } from "./Main.style";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useEffect } from "react";
+
+type FormData = {
+  firstName: string;
+  email: string;
+};
+
+const url = "/api/email-validator.php?email=";
 
 export const Main = () => {
+  const { register, watch, handleSubmit } = useForm<FormData>();
+  const emailValue = watch("email");
+
+  const submit = handleSubmit((data) => alert(JSON.stringify(data, null, 4)));
+
+  const handleValidation = async (email: string) => {
+    try {
+      const res = await axios(`${url}${email}`);
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (emailValue && emailValue.length > 0) handleValidation(emailValue);
+  }, [emailValue]);
+
   return (
     <MainWrapper>
       <MainStyle>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={submit}>
+          <label>
+            First Name
+            <input {...register("firstName")} />
+          </label>
+          <label>
+            Email
+            <input {...register("email")} />
+          </label>
+          <button onClick={submit}>Submit</button>
+        </form>
       </MainStyle>
     </MainWrapper>
   );
